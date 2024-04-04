c0cbf0e2e78dfa6b4b356bc2b4c4a37332c931ec
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-04-04 14:20:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ tweet_text = st.text_area("Entrez le tweet à analyser:", "Super service!")
 if st.button('Predict'):
     predict_url = f"{BASE_URL}/predict/"
     response = requests.post(predict_url, json={"text": tweet_text})
-
+    print("Status Code:", response.status_code)
+    print("Response Content:", response.content)
     if response.status_code == 200:
         sentiment = response.json()['sentiment']
         st.write(f'Ce tweet est : {sentiment}')
