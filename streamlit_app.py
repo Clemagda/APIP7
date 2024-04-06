b043849140d21c2fc2936d07832b626e97fc88aa
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-04-06 12:27:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 # Récupérez l'URL de base de l'API à partir des variables d'environnement ou utiliser localhost par défaut
-BASE_URL = os.getenv("API_URL", "https://p7app.azurewebsites.net")
+BASE_URL = os.getenv("API_URL", "http://p7app.azurewebsites.net")
 
 st.title('Analyse de Sentiment des Tweets')
 
