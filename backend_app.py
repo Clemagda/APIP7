a5741b97bdad495a3e0b21cb541d50269ea2b439
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-04-10 08:58:08 +0200

    Update backend_app.py

--- backend_app.py
+++ backend_app.py
@@ -46,7 +46,7 @@ class F1Score(tf.keras.metrics.Metric):
 #directory_path = "APIP7" 
 # model_path = 'G:/Mon Drive/Documents/Apprentissage/OpenClassroom/Projet_7_Analyse_de_sentiments/Models/keras_simple.keras'
 #model_path = "Models/baseline.keras"
-model_path = "Models/model
+model_path = "Models/model"
 # Charger le modèle
 
 keras_model = tf.keras.models.load_model(model_path, compile=True)
